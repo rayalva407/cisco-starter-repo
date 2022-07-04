224f5065c248ede4204a3b376f43cbf2e092da40
rayalva407 <rayalva407@gmail.com>
2022-07-04 14:59:37 -0600

    completes Ip component

--- sextant/src/components/Exhibit.js
+++ sextant/src/components/Exhibit.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Ip from './Ip'
 
 function Exhibit({heading}) {
   return (
-    <div className='exhibit rounded-3 p-3 m-2 w-25'>
+    <div className='exhibit rounded-3 p-3 m-2'>
       <p className='text-center'>{heading}</p>
-      <div className="exhibit-content rounded text-center m-2">
-        Exhibit content
+      <div className="exhibit-content rounded text-center p-3 m-2">
+        <h3>ipv4:</h3>
+        <Ip v={"v4"} />
+        <h3>ipv6:</h3>
+        <Ip v={"v6"} />
       </div>
     </div>
   )
---
+++ sextant/src/components/Ip.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+
+function Ip({v}) {
+  let url;
+
+  const [ip, setIp] = useState("")
+  console.log(v)
+  
+  v === "v4" ? url = "https://api.ipify.org?format=json" : url = "https://api64.ipify.org?format=json"
+
+  useEffect(() => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setIp(data.ip))
+  
+  }, [])
+
+  return (
+    <div>
+      {ip}
+    </div>
+  )
+}
+
+export default Ip
